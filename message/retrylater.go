dbe24dafe47d30bf2423a193fe80b414078f95c3
Xiahe Liu <Xiahe.Liu@microsoft.com>
2021-01-06 14:01:27 -0800

    pr comemnt

--- message/retrylater.go
+++ message/retrylater.go
@@ -8,6 +8,8 @@ import (
 	servicebus "github.com/Azure/azure-service-bus-go"
 )
 
+const renewLockInterval = 20 * time.Second
+
 // RetryLater waits for the given duration before retrying the processing of the message.
 // This happens in memory and does not impact servicebus message max retry limit
 func RetryLater(retryAfter time.Duration) Handler {
@@ -24,8 +26,17 @@ func (r *retryLaterHandler) Do(ctx context.Context, orig Handler, message *servi
 	select {
 	case <-ctx.Done():
 		return Error(fmt.Errorf("aborting retrylater for message. abandoning meddage %s: %w", message.ID, ctx.Err()))
+	case <-time.After(renewLockInterval):
+		err := sub.RenewLocks(ctx, message)
+		if err != nil {
+			return Error(fmt.Errorf("error renew locks, aborting retrylater for message. abandoning message %s: error: %s", message.ID, err))
+		}
+		return orig
 	case <-time.After(r.retryAfter):
-		sub.RenewLocks(ctx, message)
+		err := sub.RenewLocks(ctx, message)
+		if err != nil {
+			return Error(fmt.Errorf("error renew locks, aborting retrylater for message. abandoning message %s: error: %s", message.ID, err))
+		}
 		return orig
 	}
 }
