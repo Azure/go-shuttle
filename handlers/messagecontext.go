8dfeb05bee536a30989d280a5d282358c59828b4
Stephane Erbrech <stephane.erbrech@gmail.com>
2021-02-03 17:17:57 +1100

    doc DeadlineContext handler

--- handlers/messagecontext.go
+++ handlers/messagecontext.go
@@ -12,6 +12,7 @@ var _ servicebus.Handler = (*DeadlineContext)(nil)
 // DeadlineContext is a servicebus middleware handler that creates a message context from the incoming context to isolate
 // each message handling sub processes. It adds a deadline equal to `EnqueuedTime + TTL` to the message context if the message TTL is defined.
 // Otherwise no deadline is applied on the message context.
+// The context is canceled once the `next` middleware returns. this implies that no middleware above DeadlineContext can use this context.
 type DeadlineContext struct {
 	next servicebus.Handler
 }
@@ -21,6 +22,7 @@ func (mc *DeadlineContext) Handle(ctx context.Context, msg *servicebus.Message) 
 		return NextHandlerNilError
 	}
 	msgCtx, cancel := setupDeadline(ctx, msg)
+	// NOTE: this assumes that no handler above the DeadlineContext handler needs this context.
 	defer cancel()
 	return mc.next.Handle(msgCtx, msg)
 }
