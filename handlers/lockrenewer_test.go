3928bb671a63fed100540fd0f0dc34ebe5c680bc
Stephane Erbrech <stephane.erbrech@gmail.com>
2021-07-14 10:35:23 +1000

    add unit tests

--- handlers/lockrenewer_test.go
+++ handlers/lockrenewer_test.go
@@ -12,11 +12,19 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-type NoOpHandler struct {
+type FakeHandler struct {
+	Ctx        context.Context
+	Msg        *servicebus.Message
+	HandleFunc func(ctx context.Context, msg *servicebus.Message) error
 }
 
-func (h *NoOpHandler) Handle(_ context.Context, _ *servicebus.Message) error {
-	return nil
+func (h *FakeHandler) Handle(ctx context.Context, msg *servicebus.Message) error {
+	h.Ctx = ctx
+	h.Msg = msg
+	if h.HandleFunc == nil {
+		return nil
+	}
+	return h.HandleFunc(ctx, msg)
 }
 
 type testLockRenewer struct {
@@ -33,7 +41,7 @@ var _ handlers.LockRenewer = &testLockRenewer{}
 func Test_RenewPeriodically(t *testing.T) {
 	renewer := &testLockRenewer{}
 	interval := 50 * time.Millisecond
-	lr := handlers.NewPeekLockRenewer(&interval, renewer, &NoOpHandler{})
+	lr := handlers.NewPeekLockRenewer(&interval, renewer, &FakeHandler{})
 	msg := &servicebus.Message{}
 	ctx, _ := context.WithTimeout(context.TODO(), 120*time.Millisecond)
 	lr.Handle(ctx, msg)
@@ -47,7 +55,7 @@ func Test_RenewLockMetrics(t *testing.T) {
 	listener.Metrics.Init(reg)
 	renewer := &testLockRenewer{}
 	interval := 10 * time.Millisecond
-	lr := handlers.NewPeekLockRenewer(&interval, renewer, &NoOpHandler{})
+	lr := handlers.NewPeekLockRenewer(&interval, renewer, &FakeHandler{})
 	msg := &servicebus.Message{}
 	lr.Handle(context.TODO(), msg)
 	if !assert.Eventually(t, func() bool { return renewer.RenewCount == 2 }, 35*time.Millisecond, 5*time.Millisecond) {
@@ -68,7 +76,7 @@ func Test_RenewLockMetrics(t *testing.T) {
 func Test_RenewPeriodically_ContextCanceled(t *testing.T) {
 	renewer := &testLockRenewer{}
 	interval := 50 * time.Millisecond
-	lr := handlers.NewPeekLockRenewer(&interval, renewer, &NoOpHandler{})
+	lr := handlers.NewPeekLockRenewer(&interval, renewer, &FakeHandler{})
 	msg := &servicebus.Message{}
 	ctx, _ := context.WithTimeout(context.TODO(), 45*time.Millisecond)
 	lr.Handle(ctx, msg)
