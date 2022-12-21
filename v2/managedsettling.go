cc0103b643a327968e6d2fd7fe535fa4da494016
Stephane Erbrech <stephane.erbrech@gmail.com>
2022-12-21 15:03:28 +1100

    add ManagedSettling handler api

---
+++ v2/managedsettling.go
@@ -0,0 +1,111 @@
+package v2
+
+import (
+	"context"
+	"time"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/messaging/azservicebus"
+)
+
+// ManagedSettlingFunc is the signature of the message handler to implement when using the ManagedSettling middleware
+type ManagedSettlingFunc func(ctx context.Context, message *azservicebus.ReceivedMessage) error
+
+var _ Handler = (*ManagedSettler)(nil)
+
+// ManagedSettler is a middleware that allows to reduce the message handler signature to ManagedSettlingFunc
+type ManagedSettler struct {
+	next    ManagedSettlingFunc
+	options *ManagedSettlingOptions
+}
+
+func (m *ManagedSettler) Handle(ctx context.Context, settler MessageSettler, message *azservicebus.ReceivedMessage) {
+	if err := m.next(ctx, message); err != nil {
+		log(ctx, "error returned from the handler. Calling ManagedSettler error handler")
+		m.handleError(ctx, settler, message, err)
+		return
+	}
+	if err := settler.CompleteMessage(ctx, message, nil); err != nil {
+		log(ctx, err)
+		// if we fail to complete the message, we log the error and let the message lock expire.
+		// we cannot do more at this point.
+	}
+}
+
+// RetryDecision allows to provide custom retry decision.
+type RetryDecision interface {
+	// CanRetry inspects the error returned from the message handler, and the message itself to decide if it should be retried or not.
+	CanRetry(err error, message *azservicebus.ReceivedMessage) bool
+}
+
+// MaxAttemptsRetryDecision defines how many delivery the handler allows before explicitly moving the message to the deadletter queue.
+// This requires the MaxDeliveryCount from the queue or subscription to be higher than the MaxAttempts property.
+// If the queue or subscription's MaxDeliveryCount is lower than MaxAttempts,
+// service bus will move the message to the DLQ before the handler reaches the MaxAttempts.
+type MaxAttemptsRetryDecision struct {
+	MaxAttempts uint32
+}
+
+func (d *MaxAttemptsRetryDecision) CanRetry(_ error, message *azservicebus.ReceivedMessage) bool {
+	return message.DeliveryCount > d.MaxAttempts
+}
+
+// RetryDelayStrategy can be implemented to provide custom delay retry strategies.
+type RetryDelayStrategy interface {
+	GetDelay(deliveryCount uint32) time.Duration
+}
+
+// ConstantDelayStrategy delays the message retry by the given duration
+type ConstantDelayStrategy struct {
+	Delay time.Duration
+}
+
+func (s *ConstantDelayStrategy) GetDelay(_ uint32) time.Duration {
+	return s.Delay
+}
+
+// ManagedSettlingOptions allows to configure the ManagedSettling middleware
+type ManagedSettlingOptions struct {
+	// RetryDecision is invoked to decide whether an error should be retried.
+	// the default is to retry 5 times before moving the message to the deadletter.
+	RetryDecision RetryDecision
+	// RetryDelayStrategy is invoked when a message handling does not complete successfully
+	// and the RetryDecision decides to retry the message.
+	// The handler will sleep for the time calculated before Abandoning the message.
+	RetryDelayStrategy RetryDelayStrategy
+}
+
+func NewManagedSettlingHandler(opts *ManagedSettlingOptions, handler ManagedSettlingFunc) *ManagedSettler {
+	options := &ManagedSettlingOptions{
+		RetryDecision:      &MaxAttemptsRetryDecision{MaxAttempts: 5},
+		RetryDelayStrategy: &ConstantDelayStrategy{Delay: 5 * time.Second},
+	}
+	if opts != nil {
+		if opts.RetryDecision != nil {
+			options.RetryDecision = opts.RetryDecision
+		}
+		if opts.RetryDelayStrategy != nil {
+			options.RetryDelayStrategy = opts.RetryDelayStrategy
+		}
+	}
+	return &ManagedSettler{
+		next:    handler,
+		options: options,
+	}
+}
+
+func (m *ManagedSettler) handleError(ctx context.Context, settler MessageSettler, message *azservicebus.ReceivedMessage, handleErr error) {
+	if !m.options.RetryDecision.CanRetry(handleErr, message) {
+		deadLetterSettlement.settle(ctx, settler, message, &azservicebus.DeadLetterOptions{
+			ErrorDescription:   to.Ptr("ManagedSettlingHandlerDeadLettering"),
+			Reason:             to.Ptr(handleErr.Error()),
+			PropertiesToModify: nil,
+		})
+	}
+	// the delay is implemented as an in-memory sleep before calling abandon.
+	// this will continue renewing the lock on the message while we wait for this delay to pass.
+	delay := m.options.RetryDelayStrategy.GetDelay(message.DeliveryCount)
+	log(ctx, "delay strategy return delay of %s", delay)
+	time.Sleep(delay)
+	abandonSettlement.settle(ctx, settler, message, nil)
+}
---
+++ v2/managedsettling_example_test.go
@@ -0,0 +1,53 @@
+package v2_test
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/messaging/azservicebus"
+	shuttle "github.com/Azure/go-shuttle/v2"
+)
+
+func ExampleManagedSettlingHandler() {
+	tokenCredential, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		panic(err)
+	}
+	client, err := azservicebus.NewClient("myservicebus.servicebus.windows.net", tokenCredential, nil)
+	if err != nil {
+		panic(err)
+	}
+	receiver, err := client.NewReceiverForSubscription("topic-a", "sub-a", nil)
+	if err != nil {
+		panic(err)
+	}
+	lockRenewalInterval := 10 * time.Second
+	p := shuttle.NewProcessor(receiver,
+		shuttle.NewPanicHandler(
+			shuttle.NewRenewLockHandler(receiver, &lockRenewalInterval,
+				shuttle.NewManagedSettlingHandler(&shuttle.ManagedSettlingOptions{
+					RetryDecision:      &shuttle.MaxAttemptsRetryDecision{MaxAttempts: 2},
+					RetryDelayStrategy: &shuttle.ConstantDelayStrategy{Delay: 2 * time.Second},
+				}, myManagedSettlementHandler()))), &shuttle.ProcessorOptions{MaxConcurrency: 10})
+
+	ctx, cancel := context.WithCancel(context.Background())
+	err = p.Start(ctx)
+	if err != nil {
+		panic(err)
+	}
+	cancel()
+}
+
+func myManagedSettlementHandler() shuttle.ManagedSettlingFunc {
+	count := 0
+	return func(ctx context.Context, message *azservicebus.ReceivedMessage) error {
+		count++
+		if count == 0 {
+			// this will abandon the message
+			return fmt.Errorf("this will abandon the message, and eventually move it to DLQ")
+		}
+		return nil // this will complete the message
+	}
+}
