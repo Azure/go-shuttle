bb7631724a3941815b990a8aa3223e3e47a85b1f
Stephane <stephane.erbrech@gmail.com>
2023-07-17 09:28:08 +1000

    move logger abstraction to a separate file

---
+++ v2/logging.go
@@ -0,0 +1,36 @@
+package shuttle
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+)
+
+type Logger interface {
+	Info(s string)
+	Warn(s string)
+	Error(s string)
+}
+
+var getLogger = func(_ context.Context) Logger { return &printLogger{} }
+
+type printLogger struct{}
+
+func (l *printLogger) Info(s string) {
+	fmt.Println(append(append([]any{}, "INFO - ", time.Now().UTC(), " - "), s)...)
+}
+
+func (l *printLogger) Warn(s string) {
+	fmt.Println(append(append([]any{}, "WARN - ", time.Now().UTC(), " - "), s)...)
+}
+
+func (l *printLogger) Error(s string) {
+	fmt.Println(append(append([]any{}, "ERROR - ", time.Now().UTC(), " - "), s)...)
+}
+
+func log(ctx context.Context, a ...any) {
+	if os.Getenv("GOSHUTTLE_LOG") == "ALL" {
+		getLogger(ctx).Info(fmt.Sprint(append(append([]any{}, time.Now().UTC(), " - "), a...)...))
+	}
+}
--- v2/processor.go
+++ v2/processor.go
@@ -4,14 +4,14 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"os"
 	"time"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	"github.com/Azure/azure-sdk-for-go/sdk/messaging/azservicebus"
-	"github.com/Azure/go-shuttle/v2/metrics"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/trace"
+
+	"github.com/Azure/go-shuttle/v2/metrics"
 )
 
 type Receiver interface {
@@ -217,33 +217,5 @@ func (plr *peekLockRenewer) startPeriodicRenewal(ctx context.Context, message *a
 			}
 			alive = false
 		}
-	}
-}
-
-type Logger interface {
-	Info(s string)
-	Warn(s string)
-	Error(s string)
-}
-
-var getLogger = func(_ context.Context) Logger { return &printLogger{} }
-
-type printLogger struct{}
-
-func (l *printLogger) Info(s string) {
-	fmt.Println(append(append([]any{}, "INFO - ", time.Now().UTC(), " - "), s)...)
-}
-
-func (l *printLogger) Warn(s string) {
-	fmt.Println(append(append([]any{}, "WARN - ", time.Now().UTC(), " - "), s)...)
-}
-
-func (l *printLogger) Error(s string) {
-	fmt.Println(append(append([]any{}, "ERROR - ", time.Now().UTC(), " - "), s)...)
-}
-
-func log(ctx context.Context, a ...any) {
-	if os.Getenv("GOSHUTTLE_LOG") == "ALL" {
-		getLogger(ctx).Info(fmt.Sprint(append(append([]any{}, time.Now().UTC(), " - "), a...)...))
 	}
 }
