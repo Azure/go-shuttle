9435c5f0c885e36f0e2d6ab1c05e6e3c6b781a98
Stephane <stephane.erbrech@gmail.com>
2023-07-18 13:52:41 +1000

    cancel messsage context when lockrenewer stops

--- v2/lockrenewer.go
+++ v2/lockrenewer.go
@@ -7,6 +7,7 @@ import (
 	"sync/atomic"
 	"time"
 
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	"github.com/Azure/azure-sdk-for-go/sdk/messaging/azservicebus"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/trace"
@@ -14,29 +15,69 @@ import (
 	"github.com/Azure/go-shuttle/v2/metrics"
 )
 
-// NewRenewLockHandler starts a renewlock goroutine for each message received.
-func NewRenewLockHandler(lockRenewer LockRenewer, interval *time.Duration, handler Handler) HandlerFunc {
+// LockRenewer abstracts the servicebus receiver client to only expose lock renewal
+type LockRenewer interface {
+	RenewMessageLock(ctx context.Context, message *azservicebus.ReceivedMessage, options *azservicebus.RenewMessageLockOptions) error
+}
+
+// LockRenewalOptions configures the lock renewal.
+type LockRenewalOptions struct {
+	// Interval defines the frequency at which we renew the lock on the message. Defaults to 10 seconds.
+	Interval *time.Duration
+	// CancelMessageContextOnStop will cancel the downstream message context when the renewal handler is stopped.
+	// Defaults to true.
+	CancelMessageContextOnStop *bool
+}
+
+// NewLockRenewalHandler returns a middleware handler that will renew the lock on the message at the specified interval.
+func NewLockRenewalHandler(lockRenewer LockRenewer, options *LockRenewalOptions, handler Handler) HandlerFunc {
+	interval := 10 * time.Second
+	cancelMessageContextOnStop := true
+	if options != nil {
+		if options.Interval != nil {
+			interval = *options.Interval
+		}
+		if options.CancelMessageContextOnStop != nil {
+			cancelMessageContextOnStop = *options.CancelMessageContextOnStop
+		}
+	}
 	plr := &peekLockRenewer{
-		next:            handler,
-		lockRenewer:     lockRenewer,
-		renewalInterval: interval,
-		stopped:         make(chan struct{}, 1), // buffered channel to ensure we are not blocking
+		next:                   handler,
+		lockRenewer:            lockRenewer,
+		renewalInterval:        &interval,
+		cancelMessageCtxOnStop: cancelMessageContextOnStop,
+		stopped:                make(chan struct{}, 1), // buffered channel to ensure we are not blocking
 	}
 	return func(ctx context.Context, settler MessageSettler, message *azservicebus.ReceivedMessage) {
-		go plr.startPeriodicRenewal(ctx, message)
-		handler.Handle(ctx, settler, message)
-		plr.stop(ctx)
+		renewalCtx, cancel := context.WithCancel(ctx)
+		plr.cancelMessageCtx = cancel
+		go plr.startPeriodicRenewal(renewalCtx, message)
+		handler.Handle(renewalCtx, settler, message)
+		plr.stop(renewalCtx)
 	}
 }
 
-// PeekLockRenewer starts a background goroutine that renews the message lock at the given interval until Stop() is called
+// Deprecated: use NewLockRenewalHandler
+// NewRenewLockHandler starts a renewlock goroutine for each message received.
+func NewRenewLockHandler(lockRenewer LockRenewer, interval *time.Duration, handler Handler) HandlerFunc {
+	return NewLockRenewalHandler(lockRenewer,
+		&LockRenewalOptions{
+			Interval: interval,
+			// default to false on the old handler signature to keep the same behavior
+			CancelMessageContextOnStop: to.Ptr(false)},
+		handler)
+}
+
+// peekLockRenewer starts a background goroutine that renews the message lock at the given interval until Stop() is called
 // or until the passed in context is canceled.
 // it is a pass through handler if the renewalInterval is nil
 type peekLockRenewer struct {
-	next            Handler
-	lockRenewer     LockRenewer
-	renewalInterval *time.Duration
-	alive           atomic.Bool
+	next                   Handler
+	lockRenewer            LockRenewer
+	renewalInterval        *time.Duration
+	alive                  atomic.Bool
+	cancelMessageCtxOnStop bool
+	cancelMessageCtx       func()
 
 	// stopped channel allows to short circuit the renewal loop
 	// when we are already waiting on the select.
@@ -46,11 +87,16 @@ type peekLockRenewer struct {
 	stopped chan struct{}
 }
 
+// stop will stop the renewal loop. if LockRenewalOptions.CancelMessageContextOnStop is set to true, it cancels the message context.
 func (plr *peekLockRenewer) stop(ctx context.Context) {
 	plr.alive.Store(false)
 	// don't send the stop signal to the loop if there is already one in the channel
 	if len(plr.stopped) == 0 {
 		plr.stopped <- struct{}{}
+	}
+	if plr.cancelMessageCtxOnStop {
+		log(ctx, "canceling message context")
+		plr.cancelMessageCtx()
 	}
 	log(ctx, "stopped periodic renewal")
 }
@@ -86,7 +132,8 @@ func (plr *peekLockRenewer) startPeriodicRenewal(ctx context.Context, message *a
 				span.RecordError(fmt.Errorf("failed to renew lock: %w", err))
 				// on error, we continue to the next loop iteration.
 				// if the context is Done, we will enter the ctx.Done() case and exit the renewal.
-				// if the error is anything else, we keep retrying the renewal
+				// if the error is identified as permanent, we stop the renewal.
+				// if the error is anything else, we keep trying the renewal.
 				if plr.isPermanent(err) {
 					log(ctx, "stopping periodic renewal for message: ", message.MessageID)
 					plr.stop(ctx)
