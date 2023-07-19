39f6f6a57f816b91e49f53c49cfde875d24bd39f
Stephane <stephane.erbrech@gmail.com>
2023-07-19 20:36:26 +1000

    remove logf

--- v2/logging.go
+++ v2/logging.go
@@ -34,12 +34,8 @@ func (l *printLogger) Error(s string) {
 	fmt.Println(append(append([]any{}, "ERROR - ", time.Now().UTC(), " - "), s)...)
 }
 
-func logf(ctx context.Context, format string, a ...any) {
-	log(ctx, fmt.Sprintf(format, a...))
-}
-
 func log(ctx context.Context, a ...any) {
 	if os.Getenv("GOSHUTTLE_LOG") == "ALL" {
-		logf(ctx, fmt.Sprint(a...))
+		getLogger(ctx).Info(fmt.Sprint(a...))
 	}
 }
